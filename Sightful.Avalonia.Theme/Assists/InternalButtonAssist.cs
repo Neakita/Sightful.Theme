5662ede97df5a92f87ecde291f36747a9a28f282
KatterMaw <narcat99@gmail.com>
2024-03-27 14:11:23 +0500

    Move InternalButtonAssist HoveredBackgroundProperty & PressedBackgroundProperty to new public ButtonAssist

--- Sightful.Avalonia.Theme/Assists/InternalButtonAssist.cs
+++ Sightful.Avalonia.Theme/Assists/InternalButtonAssist.cs
@@ -1,45 +1,10 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Media;
 
 namespace Sightful.Avalonia.Theme.Assists;
 
 internal static class InternalButtonAssist
 {
-	#region HoveredBackground
-
-	public static readonly AvaloniaProperty<IBrush?> HoveredBackgroundProperty =
-		AvaloniaProperty.RegisterAttached<Button, IBrush?>("HoveredBackground", typeof(InternalButtonAssist));
-
-	public static IBrush? GetHoveredBackground(AvaloniaObject element)
-	{
-		return element.GetValue<IBrush?>(HoveredBackgroundProperty);
-	}
-
-	public static void SetHoveredBackground(AvaloniaObject element, IBrush? value)
-	{
-		element.SetValue(HoveredBackgroundProperty, value);
-	}
-
-	#endregion
-
-	#region PressedBackground
-	
-	public static readonly AvaloniaProperty<IBrush?> PressedBackgroundProperty =
-		AvaloniaProperty.RegisterAttached<Button, IBrush?>("PressedBackground", typeof(InternalButtonAssist));
-	
-	public static IBrush? GetPressedBackground(AvaloniaObject element)
-	{
-		return element.GetValue<IBrush?>(PressedBackgroundProperty);
-	}
-
-	public static void SetPressedBackground(AvaloniaObject element, IBrush? value)
-	{
-		element.SetValue(PressedBackgroundProperty, value);
-	}
-
-	#endregion
-
 	#region ShrinkingThickness
 
 	public static readonly AvaloniaProperty<Thickness> ShrinkingThicknessProperty =
