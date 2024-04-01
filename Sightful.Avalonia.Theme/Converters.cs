242adfaa8773e57dca65920ff1f500c772c0f670
KatterMaw <narcat99@gmail.com>
2024-04-02 01:48:39 +0500

    Set ExtendClientAreaTitleBarHeightHint in window theme

--- Sightful.Avalonia.Theme/Converters.cs
+++ Sightful.Avalonia.Theme/Converters.cs
@@ -7,4 +7,6 @@ internal static class Converters
 {
 	public static FuncMultiValueConverter<Thickness, Thickness> ThicknessSubtractConverter { get; } =
 		new(thicknesses => thicknesses.Aggregate((x, y) => x - y));
+	public static TwoWayFuncValueConverter<double, Rect> HeightRectConverter { get; } =
+		new(height => new Rect(0, 0, 0, height), rect => rect.Height);
 }
