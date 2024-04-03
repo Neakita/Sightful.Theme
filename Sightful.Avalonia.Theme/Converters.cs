9f9ee8190dd35405c7205c96e11951311817b860
KatterMaw <narcat99@gmail.com>
2024-04-03 19:16:40 +0500

    Compensate BorderThickness in TextBox

--- Sightful.Avalonia.Theme/Converters.cs
+++ Sightful.Avalonia.Theme/Converters.cs
@@ -9,4 +9,6 @@ internal static class Converters
 		new(thicknesses => thicknesses.Aggregate((x, y) => x - y));
 	public static TwoWayFuncValueConverter<double, Rect> HeightRectConverter { get; } =
 		new(height => new Rect(0, 0, 0, height), rect => rect.Height);
+	public static FuncMultiValueConverter<Thickness, Thickness> ThicknessMultiplicationConverter { get; } =
+		new(thicknesses => thicknesses.Aggregate((x, y) => new Thickness(x.Left * y.Left, x.Top * y.Top, x.Right * y.Right, x.Bottom * y.Bottom)));
 }
