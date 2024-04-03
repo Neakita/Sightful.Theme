59b586e3a79d58bf4d92cc7eee05a714ca90f83a
KatterMaw <narcat99@gmail.com>
2024-04-03 19:00:40 +0500

    Extract static margin from button to resource in SpatialTheme

--- Sightful.Avalonia.Theme/SpatialTheme.cs
+++ Sightful.Avalonia.Theme/SpatialTheme.cs
@@ -10,6 +10,7 @@ public class SpatialTheme : ResourceDictionary
 	public virtual double ControlsHeight => 40;
 	public virtual double WideControlsHeight => 48;
 	public virtual Thickness ButtonPadding => new(16, 8);
+	public virtual Thickness ControlsMargin => new(6);
 
 	public void Initialize()
 	{
@@ -24,6 +25,7 @@ public class SpatialTheme : ResourceDictionary
 		AddResource(ControlsHeight);
 		AddResource(WideControlsHeight);
 		AddResource(ButtonPadding);
+		AddResource(ControlsMargin);
 	}
 
 	private void AddResource<T>(T value, [CallerArgumentExpression(nameof(value))] string key = "")
