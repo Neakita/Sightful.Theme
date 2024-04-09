1078ca47fbe3e421dc1fbcfbc421391461277a2f
KatterMaw <narcat99@gmail.com>
2024-04-09 05:31:26 +0500

    Several styling improvements

--- Sightful.Avalonia.Theme/WindowInnerContentCornerRadiusConverter.cs
+++ Sightful.Avalonia.Theme/WindowInnerContentCornerRadiusConverter.cs
@@ -4,6 +4,7 @@ using Avalonia.Data.Converters;
 
 namespace Sightful.Avalonia.Theme;
 
+// It is MultiValue for possible future extension
 internal sealed class WindowInnerContentCornerRadiusConverter : IMultiValueConverter
 {
 	public object Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
