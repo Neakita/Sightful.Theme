495b5276f240e9b34f45bad41b2e77d34c3a0c0d
KatterMaw <narcat99@gmail.com>
2024-04-23 16:13:30 +0500

    Add ListBox

---
+++ Sightful.Avalonia.Theme/Assists/InternalListBoxItemAssist.cs
@@ -0,0 +1,25 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace Sightful.Avalonia.Theme.Assists;
+
+internal static class InternalListBoxItemAssist
+{
+	#region Fade
+
+	public static readonly AvaloniaProperty<IBrush?> FadeProperty =
+		AvaloniaProperty.RegisterAttached<Button, IBrush?>("Fade", typeof(ButtonAssist));
+
+	public static IBrush? GetFade(AvaloniaObject element)
+	{
+		return element.GetValue<IBrush?>(FadeProperty);
+	}
+
+	public static void SetFade(AvaloniaObject element, IBrush? value)
+	{
+		element.SetValue(FadeProperty, value);
+	}
+
+	#endregion
+}
