d29a73fdceac3394202a06a13f26e3f0169e9aee
KatterMaw <narcat99@gmail.com>
2024-05-17 19:28:01 +0500

    Adjust SquareButtonPadding

--- Sightful.Avalonia/SpatialTheme.axaml.cs
+++ Sightful.Avalonia/SpatialTheme.axaml.cs
@@ -12,7 +12,7 @@ public class SpatialTheme : ResourceDictionary
 	public virtual double ControlsHeight => 40;
 	public virtual double WideControlsHeight => 48;
 	public virtual Thickness ButtonPadding => new(16, 8);
-	public virtual Thickness SquareButtonPadding => new(10);
+	public virtual Thickness SquareButtonPadding => new(8);
 	public virtual Thickness WideSquareButtonPadding => new(12);
 	public virtual Thickness ControlsMargin => new(6);
 	public virtual double FontSize => 18;
