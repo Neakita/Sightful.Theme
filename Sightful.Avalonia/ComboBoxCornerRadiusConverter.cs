f0e09e17878cb7ee9af68fa84f7b79c7166e67a0
Katter <narcat99@gmail.com>
2024-09-25 16:18:34 +0500

    ComboBox theme improvements

--- Sightful.Avalonia/ComboBoxCornerRadiusConverter.cs
+++ Sightful.Avalonia/ComboBoxCornerRadiusConverter.cs
@@ -11,13 +11,21 @@ internal sealed class ComboBoxCornerRadiusConverter : IMultiValueConverter
 {
 	public object Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
 	{
-		Guard.IsEqualTo(values.Count, 3);
+		Guard.IsEqualTo(values.Count, 4);
 		if (values[0] is not CornerRadius cornerRadius ||
 		    values[1] is not bool isPopupOpen ||
-		    values[2] is not PlacementMode placement)
+		    values[2] is not PlacementMode placement ||
+		    values[3] is not bool invert)
 			return new BindingNotification(new InvalidCastException(), BindingErrorType.Error);
 		if (!isPopupOpen)
 			return cornerRadius;
+		if (invert)
+			placement = placement switch
+			{
+				PlacementMode.Top => PlacementMode.Bottom,
+				PlacementMode.Bottom => PlacementMode.Top,
+				_ => placement
+			};
 		return placement switch
 		{
 			PlacementMode.Top => new CornerRadius(0, 0, cornerRadius.BottomRight, cornerRadius.BottomLeft),
