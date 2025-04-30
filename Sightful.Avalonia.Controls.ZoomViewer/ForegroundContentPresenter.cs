ad635fd2ff9179442a47e6b93c8d2f8084a68dd9
Neakita <thekatterdev@gmail.com>
2025-04-30 15:29:32 +0500

    ZoomViewer.ForegroundContent

---
+++ Sightful.Avalonia.Controls.ZoomViewer/ForegroundContentPresenter.cs
@@ -0,0 +1,53 @@
+using Avalonia;
+using Avalonia.Controls.Presenters;
+
+namespace Sightful.Avalonia.Controls.ZoomViewer;
+
+internal sealed class ForegroundContentPresenter : ContentPresenter
+{
+	public static readonly StyledProperty<Size> ContentSizeProperty =
+		AvaloniaProperty.Register<ForegroundContentPresenter, Size>(nameof(ContentSize));
+
+	public static readonly StyledProperty<double> ScaleProperty =
+		ZoomViewer.ZoomProperty.AddOwner<ForegroundContentPresenter>();
+
+	public static readonly StyledProperty<Vector> OffsetProperty =
+		ZoomViewer.OffsetProperty.AddOwner<ForegroundContentPresenter>();
+
+	static ForegroundContentPresenter()
+	{
+		AffectsArrange<ForegroundContentPresenter>(ScaleProperty, OffsetProperty);
+	}
+
+	public Size ContentSize
+	{
+		get => GetValue(ContentSizeProperty);
+		set => SetValue(ContentSizeProperty, value);
+	}
+
+	public double Scale
+	{
+		get => GetValue(ScaleProperty);
+		set => SetValue(ScaleProperty, value);
+	}
+
+	public Vector Offset
+	{
+		get => GetValue(OffsetProperty);
+		set => SetValue(OffsetProperty, value);
+	}
+
+	protected override Size ArrangeOverride(Size finalSize)
+	{
+		if (Child == null)
+			return finalSize;
+		var center = new Point(finalSize.Width / 2, finalSize.Height / 2);
+		var scaledSize = ContentSize * Scale;
+		var topLeft = new Point(
+			center.X - scaledSize.Width / 2 + Offset.X * Scale,
+			center.Y - scaledSize.Height / 2 + Offset.Y * Scale);
+		var bounds = new Rect(topLeft, scaledSize);
+		Child.Arrange(bounds);
+		return finalSize;
+	}
+}
--- Sightful.Avalonia.Controls.ZoomViewer/ZoomViewer.axaml.cs
+++ Sightful.Avalonia.Controls.ZoomViewer/ZoomViewer.axaml.cs
@@ -1,10 +1,17 @@
+using System.Reactive.Linq;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
+using Avalonia.Controls.Presenters;
+using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Templates;
 using Avalonia.Data;
 using Avalonia.Input;
 
 namespace Sightful.Avalonia.Controls.ZoomViewer;
 
+[TemplatePart(ContentPresenterName, typeof(ContentPresenter))]
+[TemplatePart(ForegroundContentPresenterName, typeof(ContentPresenter))]
 public sealed class ZoomViewer : ContentControl
 {
 	public static readonly StyledProperty<double> ZoomProperty =
@@ -12,6 +19,15 @@ public sealed class ZoomViewer : ContentControl
 
 	public static readonly StyledProperty<Vector> OffsetProperty =
 		AvaloniaProperty.Register<ZoomViewer, Vector>(nameof(Offset), defaultBindingMode: BindingMode.TwoWay);
+
+	public static readonly StyledProperty<object?> ForegroundContentProperty =
+		AvaloniaProperty.Register<ZoomViewer, object?>(nameof(ForegroundContent));
+
+	public static readonly StyledProperty<IDataTemplate?> ForegroundContentTemplateProperty =
+		AvaloniaProperty.Register<ZoomViewer, IDataTemplate?>(nameof(ForegroundContentTemplate));
+
+	private const string ContentPresenterName = "PART_ContentPresenter";
+	private const string ForegroundContentPresenterName = "PART_ForegroundContentPresenter";
 
 	private static double CoerceZoom(AvaloniaObject element, double zoom)
 	{
@@ -30,6 +46,35 @@ public sealed class ZoomViewer : ContentControl
 		set => SetValue(OffsetProperty, value);
 	}
 
+	public object? ForegroundContent
+	{
+		get => GetValue(ForegroundContentProperty);
+		set => SetValue(ForegroundContentProperty, value);
+	}
+
+	public IDataTemplate? ForegroundContentTemplate
+	{
+		get => GetValue(ForegroundContentTemplateProperty);
+		set => SetValue(ForegroundContentTemplateProperty, value);
+	}
+
+	protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+	{
+		_contentSizeBinding?.Dispose();
+		var contentPresenter = e.NameScope.Find<ContentPresenter>(ContentPresenterName);
+		var foregroundContentPresenter = e.NameScope.Find<ContentPresenter>(ForegroundContentPresenterName);
+		if (contentPresenter == null || foregroundContentPresenter == null)
+			return;
+		var contentSizeObservable = contentPresenter
+			.GetObservable(ContentPresenter.ChildProperty)
+			.Where(child => child != null)
+			.Select(child => child!)
+			.Select(child => child.GetObservable(BoundsProperty))
+			.Switch()
+			.Select(bounds => bounds.Size);
+		_contentSizeBinding = foregroundContentPresenter.Bind(ForegroundContentPresenter.ContentSizeProperty, contentSizeObservable);
+	}
+
 	protected override void OnPointerWheelChanged(PointerWheelEventArgs e)
 	{
 		SetCurrentValue(ZoomProperty, Zoom += e.Delta.Y);
@@ -42,6 +87,7 @@ public sealed class ZoomViewer : ContentControl
 		PointerReleased += OnPointerReleased;
 	}
 
+	private IDisposable? _contentSizeBinding;
 	private Point _lastPanPosition;
 
 	private void OnPointerMoved(object? sender, PointerEventArgs e)
