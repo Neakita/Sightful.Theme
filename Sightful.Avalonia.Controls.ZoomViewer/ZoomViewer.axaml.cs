993ca24e22a4a54750e84f906df7f6846d969725
Neakita <thekatterdev@gmail.com>
2025-04-30 15:56:32 +0500

    Constraint Offset

--- Sightful.Avalonia.Controls.ZoomViewer/ZoomViewer.axaml.cs
+++ Sightful.Avalonia.Controls.ZoomViewer/ZoomViewer.axaml.cs
@@ -18,7 +18,7 @@ public sealed class ZoomViewer : ContentControl
 		AvaloniaProperty.Register<ZoomViewer, double>(nameof(Zoom), 1, defaultBindingMode: BindingMode.TwoWay, coerce: CoerceZoom);
 
 	public static readonly StyledProperty<Vector> OffsetProperty =
-		AvaloniaProperty.Register<ZoomViewer, Vector>(nameof(Offset), defaultBindingMode: BindingMode.TwoWay);
+		AvaloniaProperty.Register<ZoomViewer, Vector>(nameof(Offset), defaultBindingMode: BindingMode.TwoWay, coerce: CoerceOffset);
 
 	public static readonly StyledProperty<object?> ForegroundContentProperty =
 		AvaloniaProperty.Register<ZoomViewer, object?>(nameof(ForegroundContent));
@@ -26,12 +26,27 @@ public sealed class ZoomViewer : ContentControl
 	public static readonly StyledProperty<IDataTemplate?> ForegroundContentTemplateProperty =
 		AvaloniaProperty.Register<ZoomViewer, IDataTemplate?>(nameof(ForegroundContentTemplate));
 
+	public static readonly DirectProperty<ZoomViewer, Vector> MinOffsetProperty =
+		AvaloniaProperty.RegisterDirect<ZoomViewer, Vector>(nameof(MinOffset), viewer => viewer.MinOffset);
+
+	public static readonly DirectProperty<ZoomViewer, Vector> MaxOffsetProperty =
+		AvaloniaProperty.RegisterDirect<ZoomViewer, Vector>(nameof(MaxOffset), viewer => viewer.MaxOffset);
+
 	private const string ContentPresenterName = "PART_ContentPresenter";
 	private const string ForegroundContentPresenterName = "PART_ForegroundContentPresenter";
 
 	private static double CoerceZoom(AvaloniaObject element, double zoom)
 	{
 		return Math.Max(zoom, 1);
+	}
+
+	private static Vector CoerceOffset(AvaloniaObject element, Vector offset)
+	{
+		var (minX, minY) = element.GetValue(MinOffsetProperty);
+		var (maxX, maxY) = element.GetValue(MaxOffsetProperty);
+		return new Vector(
+			Math.Clamp(offset.X, minX, maxX),
+			Math.Clamp(offset.Y, minY, maxY));
 	}
 
 	public double Zoom
@@ -58,14 +73,26 @@ public sealed class ZoomViewer : ContentControl
 		set => SetValue(ForegroundContentTemplateProperty, value);
 	}
 
+	public Vector MinOffset
+	{
+		get => _minOffset;
+		private set => SetAndRaise(MinOffsetProperty, ref _minOffset, value);
+	}
+
+	public Vector MaxOffset
+	{
+		get => _maxOffset;
+		private set => SetAndRaise(MaxOffsetProperty, ref _maxOffset, value);
+	}
+
 	protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
 	{
 		_contentSizeBinding?.Dispose();
-		var contentPresenter = e.NameScope.Find<ContentPresenter>(ContentPresenterName);
+		_contentPresenter = e.NameScope.Find<ContentPresenter>(ContentPresenterName);
 		var foregroundContentPresenter = e.NameScope.Find<ContentPresenter>(ForegroundContentPresenterName);
-		if (contentPresenter == null || foregroundContentPresenter == null)
+		if (_contentPresenter == null || foregroundContentPresenter == null)
 			return;
-		var contentSizeObservable = contentPresenter
+		var contentSizeObservable = _contentPresenter
 			.GetObservable(ContentPresenter.ChildProperty)
 			.Where(child => child != null)
 			.Select(child => child!)
@@ -73,6 +100,24 @@ public sealed class ZoomViewer : ContentControl
 			.Switch()
 			.Select(bounds => bounds.Size);
 		_contentSizeBinding = foregroundContentPresenter.Bind(ForegroundContentPresenter.ContentSizeProperty, contentSizeObservable);
+	}
+
+	protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+	{
+		base.OnPropertyChanged(change);
+		if (change.Property == ZoomProperty)
+		{
+			if (_contentPresenter == null || _contentPresenter.Child == null)
+				return;
+			var viewport = _contentPresenter.Bounds.Size;
+			var contentSize = _contentPresenter.Child.Bounds.Size * Zoom;
+			var excess = contentSize - viewport;
+			excess = new Size(
+				Math.Max(0, excess.Width),
+				Math.Max(0, excess.Height));
+			MaxOffset = new Vector(excess.Width / 2 / Zoom, excess.Height / 2 / Zoom);
+			MinOffset = -MaxOffset;
+		}
 	}
 
 	protected override void OnPointerWheelChanged(PointerWheelEventArgs e)
@@ -87,8 +132,11 @@ public sealed class ZoomViewer : ContentControl
 		PointerReleased += OnPointerReleased;
 	}
 
+	private ContentPresenter? _contentPresenter;
 	private IDisposable? _contentSizeBinding;
 	private Point _lastPanPosition;
+	private Vector _minOffset;
+	private Vector _maxOffset;
 
 	private void OnPointerMoved(object? sender, PointerEventArgs e)
 	{
